30101fc09f82b92d628f4a6a8cd37a7673b54264
kok1eee <131941190+kok1eee@users.noreply.github.com>
2024-03-28 00:41:35 +0900

    3/28-2

--- python_work/6.dictionary/cities.py
+++ python_work/6.dictionary/cities.py
@@ -1,16 +1,19 @@
 tokyo = {
+    'name':'tokyo',
     'country':'japan',
     'population':'13000000',
     'fact':'serious'
     }
 
 california = {
+    'name':'california',
     'country':'usa',
     'population':'39000000',
     'fact':'friendly',
     }
 
 beijing = {
+    'name':'beijing',
     'country':'china',
     'population':'21000000',
     'fact':'egoistic'
@@ -19,4 +22,4 @@ beijing = {
 cities = [tokyo,california,beijing]
 
 for city in cities:
-    print(f"{city}は{city['country'].title()}の街で人口は{city['population']}人います。人々の性格は{city['fact']}な人が多いです。")
+    print(f"{city['name']}は{city['country'].title()}の街で人口は{city['population']}人います。人々の性格は{city['fact']}な人が多いです。")

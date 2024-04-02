56906b81fa50ba321e1b1e516023981463bc26da
kok1eee <131941190+kok1eee@users.noreply.github.com>
2024-04-03 06:19:33 +0900

    4/2

--- python_work/7.while/movie.py
+++ python_work/7.while/movie.py
@@ -1,1 +1,19 @@
-prompt = "いらっしゃいませ、"
+prompt = "いらっしゃいませ、お客様の年齢をお伺いします。"
+prompt += "人数の入力が終わりましたら'終了'をお願いします。"
+
+while True:
+    old = input(prompt)
+    old = int(old)
+
+    if old == '終了':
+        break
+    elif old <= 3:
+        print(f"{old}歳は無料になります。")
+    elif old <= 12:
+        print(f"{old}歳は子供料金で1000円になります。")
+    elif old < 60:
+        print(f"{old}歳は大人料金で1500円になります。")
+    elif old >=60:
+        print(f"{old}歳はシニア料金で1000円になります")
+    else:
+        print(f"ちゃんと入力してください。")

8f81fe53bb6b03a0b927e8067e6116c3d52b7d67
kok1eee <131941190+kok1eee@users.noreply.github.com>
2024-04-18 10:30:13 +0900

    4/18

--- python_work/11.test/test_employee.py
+++ python_work/11.test/test_employee.py
@@ -1,18 +1,28 @@
 import unittest
-from employee import Employee
+from employee import Employee as E
 
 class TestEmployee(unittest.TestCase):
     """Employeeクラスをテストする"""
-
-    def setup(self):
-        """全テストメソッドで使用する"""
-
+    def setUp(self):
+        self.person = E('masayoshi', 'tazawa', 3000000)
+        
     def test_give_default_raise(self):
         """デフォルトの状態で500000が正しく足されているか"""
-
+        self.number = ""
+        if self.number:
+            self.person.income += self.number
+        else:
+            self.person.income += 500_000
+        self.assertEqual(self.person.income, 3500000)
 
     def test_give_custom_raise(self):
         """指定された金額が正しく足されているか"""
+        self.number = 1000000
+        if self.number:
+            self.person.income += self.number
+        else:
+            self.person.income += 500_000
+        self.assertEqual(self.person.income, 4000000)
 
 if __name__ == '__main__':
     unittest.main()
